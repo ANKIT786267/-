783b9edd8fbe1506bcdfb7038e4039d78447ea85
ANKIT <95170216+ANKIT786267@users.noreply.github.com>
2022-06-21 22:27:05 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -617,7 +617,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             ]
             reply_markup = InlineKeyboardMarkup(buttons)
             await query.message.edit_reply_markup(reply_markup)
-    await query.answer('Aηᴋɪᴛ)
+    await query.answer('Aηᴋɪᴛ')
 
 
 async def auto_filter(client, msg, spoll=False):
